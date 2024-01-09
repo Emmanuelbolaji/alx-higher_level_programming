6427c2bba46217925adfceafc1633e517c775956
Emmanuelbolaji <osundiranemmanuel@gmail.com>
2024-01-09 00:14:59 -0800

    py

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -2,7 +2,7 @@
 def element_at(my_list, idx):
     if idx < len(my_list):
         return my_list[idx]
+    if idx == 0:
+        return my_list[idx]
     if idx < 0:
-        return None
-    else:
         return None
