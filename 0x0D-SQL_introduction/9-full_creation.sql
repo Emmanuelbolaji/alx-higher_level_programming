e765ccffa4e6358e6bacd5f9df69e5fe2c8ac0db
Emmanuelbolaji <osundiranemmanuel@gmail.com>
2024-03-12 11:44:04 -0700

    sql

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,6 +1,6 @@
 -- a script that creates a table second_table
 INSERT INTO second_table (id, name, score)
-VALUE(1, 'John', 10),
+VALUES(1, 'John', 10),
 (2, 'Alex', 3),
 (3, 'Bob', 14),
 (4, 'George', 8);
