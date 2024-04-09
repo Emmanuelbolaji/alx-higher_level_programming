0676bed7a82390cc00be256c43b41d5f4edc007c
Emmanuelbolaji <osundiranemmanuel@gmail.com>
2024-04-08 22:57:22 -0700

    javascript

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,14 +1,9 @@
 #!/usr/bin/node
-const argnum = process.argv.length - 2
-if (argnum === 0)
-{
-	console.log('No argument');
-}
-else if (argnum === 1)
-{
-	console.log('Argument found');
-}
-else if (argnum > 1)
-{
-	console.log('Arguments found');
+const argnum = process.argv.length - 2;
+if (argnum === 0) {
+  console.log('No argument');
+} else if (argnum === 1) {
+  console.log('Argument found');
+} else if (argnum > 1) {
+  console.log('Arguments found');
 }
