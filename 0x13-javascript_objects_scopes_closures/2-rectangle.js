13c9a1db260b1513d3037f10e625bee4644e4756
Emmanuelbolaji <osundiranemmanuel@gmail.com>
2024-04-10 10:27:00 -0700

    oblects in javascript

---
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (width, height) {
+    this.width = width;
+    this.height = height;
+  }
+}
+
+if (this.width === 0 || this.width < 0) {
+	let rectangle1 = new Rectangle();
+}
+if (this.height === 0 || this.height < 0) {
+	let rectangle2 = new Rectangle();
+}
+
+module.exports = Rectangle;
