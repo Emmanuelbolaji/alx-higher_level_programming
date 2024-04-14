cf27d1d957354b57ef6ec52477d01c9cc5cc33c3
Emmanuelbolaji <osundiranemmanuel@gmail.com>
2024-04-14 11:53:18 -0700

    sql

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,8 +1,26 @@
 #!/usr/bin/python3
+"""
+Script to list all states from a MySQL database.
+
+This script connects to a MySQL server
+The script takes three arguments
+Results are sorted in ascending order by states.id.
+
+Usage:
+    ./0-select_states.py root root hbtn_0e_0_usa
+
+Output:
+    (1, 'California')
+    (2, 'Arizona')
+    (3, 'Texas')
+    (4, 'New York')
+    (5, 'Nevada')
+"""
 import MySQLdb
 import sys
 
-if __name__ == "__main__":
+
+def main():
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
@@ -21,3 +39,7 @@ if __name__ == "__main__":
     for row in results:
         print(row)
     db.close()
+
+
+if __name__ == "__main__":
+    main()
