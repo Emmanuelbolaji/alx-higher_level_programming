e7794ce72f807eac49ffad6e214ea7d24f3eeb65
Emmanuelbolaji <osundiranemmanuel@gmail.com>
2024-04-29 02:13:36 -0700

    "Implemented Bash script to display body of 200 status code response"

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 # This script takes in a URL, sends a GET request to the URL, and displays the body of the response
-curl -s -o /tmp/response_body -w "%{http_code}" "$1" > /dev/null && grep -q "200" /tmp/response_body && tail -n +2 /tmp/response_body rm /tmp/response_bod
+curl -s -o /tmp/response_body -w "%{http_code}" "$1" > /dev/null && grep -q "200" /tmp/response_body && tail -n +2 /tmp/response_body
+rm /tmp/response_bod
